95ec1173a8eaf3af2dba72454eb227b1fdbd9199
dark2zhao <88078675+dark2zhao@users.noreply.github.com>
2023-09-08 18:38:11 +0800

    init

---
+++ Exmaple/DarkZhao.DKUIKit.example/DarkZhao.DKUIKit.example/ScollLabel.swift
@@ -0,0 +1,20 @@
+//
+//  ScollLabel.swift
+//  DarkZhao.DKUIKit.example
+//
+//  Created by zhaoboqing on 2023/9/8.
+//
+
+import UIKit
+
+class ScollLabel: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Exmaple/DarkZhao.DKUIKit.example/DarkZhao.DKUIKit.example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DarkZhao.DKUIKit.example
+//
+//  Created by zhaoboqing on 2023/9/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
